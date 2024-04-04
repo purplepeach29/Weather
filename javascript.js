7ae970534957fc623f85be0cacc1507f4583256b
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-04-04 23:22:15 +0530

    Create query selectors

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const searchBar = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-button');
+
+const locationElement = document.querySelector('.city-name');
+const weatherElement = document.querySelector('.weather');
+const temperatureElement = document.querySelector('.temperature');
+const weatherIcon = document.querySelector('.weather-icon');
+const rainElement = document.querySelector('.rain');
+
+const weatherDay1 = document.querySelector('.weather-day1');
+const weatherDay2 = document.querySelector('.weather-day2');
+const weatherDay3 = document.querySelector('.weather-day3');
+
+const temperatureDay1 = document.querySelector('.temperature-day1');
+const temperatureDay2 = document.querySelector('.temperature-day2');
+const temperatureDay3 = document.querySelector('.temperature-day3');
+
+const weatherIconDay1 = document.querySelector('.weather-icon-day1');
+const weatherIconDay2 = document.querySelector('.weather-icon-day2');
+const weatherIconDay3 = document.querySelector('.weather-icon-day3');
+
+const dayElements = document.querySelectorAll('.day');
